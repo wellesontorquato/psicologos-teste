449d9bdff593eef2be8bd511f97b07fce67f11c2
wellesontorquato <welleson09@hotmail.com>
2025-04-28 19:53:57 -0300

    fix: define Vite build directory para buscar manifest.json corretamente

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Vite; // <-- adiciona isso
 use App\Models\Evolucao;
 use Illuminate\Pagination\Paginator;
 
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Faz o Vite usar o caminho correto para o manifest
+        Vite::useBuildDirectory('build/.vite'); // <-- adiciona isso
+
         Route::model('evolucao', Evolucao::class);
         Paginator::useBootstrap();
     }
