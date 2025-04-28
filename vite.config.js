59a0d6ae33820585d65b866786c5e6bbd14bc4d8
wellesontorquato <welleson09@hotmail.com>
2025-04-28 20:41:37 -0300

    fix: ajustes finais

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -9,32 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: true,
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            output: {
-                manualChunks: undefined,
-            },
-        },
-        manifestTransforms: [
-            (manifest) => {
-                for (const key in manifest) {
-                    if (manifest[key].file) {
-                        manifest[key].file = manifest[key].file.replace('.vite/', '');
-                    }
-                    if (manifest[key].src) {
-                        manifest[key].src = manifest[key].src.replace('.vite/', '');
-                    }
-                }
-                return manifest;
-            },
-        ],
-    },
-    publicDir: false,
 });
