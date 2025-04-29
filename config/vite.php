ea72c8d26b1616910da4cb34d65ae5b715d27161
wellesontorquato <welleson09@hotmail.com>
2025-04-28 21:09:10 -0300

    fix: caminho do manifest corrigido para produção

---
+++ config/vite.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'manifest_path' => 'build/manifest.json',
+
+    'hot_file' => storage_path('app/vite.hot'),
+
+    'build_directory' => 'build',
+
+    'dev_server' => [
+        'url' => 'http://localhost:5173',
+    ],
+
+];
