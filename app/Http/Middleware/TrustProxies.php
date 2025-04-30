2b7dad6ad89c16eae0870c3ad6fe3605da7af21e
wellesontorquato <welleson09@hotmail.com>
2025-04-30 18:09:40 -0300

    Adicionar TrustProxies.php

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * Use ['*'] to trust all proxies or configure a specific list.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = SymfonyRequest::HEADER_X_FORWARDED_ALL;
+}
