d7b1788503e1bd01d959a33be1935294c3e671a6
wellesontorquato <welleson09@hotmail.com>
2025-04-30 21:02:20 -0300

    Ajustes finais para e-mail funcionar

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -11,31 +11,24 @@ class CustomVerifyEmail extends BaseVerifyEmail
 {
     public function toMail($notifiable)
     {
-        $verificationUrl = $this->verificationUrl($notifiable);
-
-        return (new MailMessage)
-            ->subject('Confirme seu e-mail para ativar sua conta no PsiGestor')
-            ->markdown('emails.verify', [
-                'verificationUrl' => $verificationUrl,
-                'user' => $notifiable,
-            ]);
-    }
-
-    protected function verificationUrl($notifiable)
-    {
-        $baseUrl = config('app.url_verification') ?? config('app.url');
-
-        return URL::temporarySignedRoute(
+        $signedUrl = URL::temporarySignedRoute(
             'verification.verify',
             Carbon::now()->addMinutes(60),
             [
                 'id' => $notifiable->getKey(),
                 'hash' => sha1($notifiable->getEmailForVerification()),
             ],
-            false // esse false evita que ele adicione APP_URL automaticamente
+            false // Gera sem usar o app.url
         );
 
-        // Agora substitui manualmente a base
-        return str_replace(config('app.url'), $baseUrl, $signedUrl);
+        $customUrl = str_replace(config('app.url'), config('app.url_verification'), $signedUrl);
+
+        return (new MailMessage)
+            ->subject('Confirme seu e-mail para ativar sua conta no PsiGestor')
+            ->markdown('emails.verify', [
+                'verificationUrl' => $customUrl,
+                'user' => $notifiable,
+            ]);
     }
 }
+
--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-# Olá! Bem-vindo ao PsiGestor, {{ $user->email }}!
+# Bem-vindo ao PsiGestor, {{ $user->email }}!
 
-Obrigado por se cadastrar em nossa plataforma.
+Olá! Obrigado por se cadastrar em nossa plataforma.
 
 Por favor, clique no botão abaixo para verificar seu endereço de e-mail e ativar sua conta:
 
@@ -19,20 +19,19 @@ Se você não criou esta conta, nenhuma ação é necessária.
     <tr>
         <td>
             <a href="https://www.instagram.com/psigestor/" target="_blank">
-                <img src="https://png.pngtree.com/element_our/png/20181011/instagram-social-media-icon-design-template-vector-png_126996.png" alt="Instagram" width="32" style="margin-right:10px;">
+                <img src="https://png.pngtree.com/element_our/png/20181011/instagram-social-media-icon-design-template-vector-png_126996.png" alt="Instagram" width="32" height="32" style="margin-right:10px;">
             </a>
         </td>
         <td>
             <a href="https://www.facebook.com/psigestor" target="_blank">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png" alt="Facebook" width="32" style="margin-right:10px;">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png" alt="Facebook" width="32" height="32" style="margin-right:10px;">
             </a>
         </td>
         <td>
             <a href="https://twitter.com/psigestor" target="_blank">
-                <img src="https://static.vecteezy.com/system/resources/previews/042/148/611/non_2x/new-twitter-x-logo-twitter-icon-x-social-media-icon-free-png.png" alt="Twitter" width="32">
+                <img src="https://static.vecteezy.com/system/resources/previews/042/148/611/non_2x/new-twitter-x-logo-twitter-icon-x-social-media-icon-free-png.png" alt="Twitter" width="32" height="32">
             </a>
         </td>
     </tr>
 </table>
-
 @endcomponent
