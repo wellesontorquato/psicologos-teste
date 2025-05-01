0dc8df66abe7a3439914e2468d6dd379c6709ced
wellesontorquato <welleson09@hotmail.com>
2025-05-01 18:56:05 -0300

    Teste 2 debug

--- routes/auth.php
+++ routes/auth.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\ValidateSignatureIgnoringScheme;
+use App\Http\Middleware\DebugSignature;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -44,8 +45,6 @@ Route::middleware('auth')->group(function () {
         ->middleware(['debug.signed', 'throttle:6,1'])
         ->name('verification.verify');
     
-
-
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
         ->middleware('throttle:6,1')
         ->name('verification.send');
