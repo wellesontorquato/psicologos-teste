a1e54640753a6982f2c25cd0cc0832671236fa74
wellesontorquato <welleson09@hotmail.com>
2025-05-02 02:04:24 -0300

    Ajustes home

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -23,17 +23,17 @@ Se você não criou esta conta, nenhuma ação é necessária.
     <tr>
         <td>
             <a href="https://www.instagram.com/psigestor/" target="_blank">
-                <img src="https://png.pngtree.com/element_our/png/20181011/instagram-social-media-icon-design-template-vector-png_126996.png" alt="Instagram" width="32" height="32" style="margin-right:10px;">
+                <img src="https://psicologos-teste-production.up.railway.app/images/instagram.png" alt="Instagram" width="32" height="32" style="margin-right:10px;">
             </a>
         </td>
         <td>
             <a href="https://www.facebook.com/psigestor" target="_blank">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png" alt="Facebook" width="32" height="32" style="margin-right:10px;">
+                <img src="https://psicologos-teste-production.up.railway.app/images/facebook.png" alt="Facebook" width="32" height="32" style="margin-right:10px;">
             </a>
         </td>
         <td>
             <a href="https://twitter.com/psigestor" target="_blank">
-                <img src="https://static.vecteezy.com/system/resources/previews/042/148/611/non_2x/new-twitter-x-logo-twitter-icon-x-social-media-icon-free-png.png" alt="Twitter" width="32" height="32">
+                <img src="https://psicologos-teste-production.up.railway.app/images/twitter.png" alt="Twitter" width="32" height="32">
             </a>
         </td>
     </tr>
