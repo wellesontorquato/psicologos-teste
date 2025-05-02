afe2e59e35656585789851ef91ac61d27ea971cd
wellesontorquato <welleson09@hotmail.com>
2025-05-02 03:59:10 -0300

    ajustando favicon

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -21,6 +21,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
     <style>
         body {
             background-color: #f0f4f8;
