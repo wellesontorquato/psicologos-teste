6f61c43f0a870a734881a90415641921e18e685d
wellesontorquato <welleson09@hotmail.com>
2025-05-04 01:55:43 -0300

    Ajuste EnviarLembretesDiarios.php

--- app/Console/Commands/EnviarLembretesDiarios.php
+++ app/Console/Commands/EnviarLembretesDiarios.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Http\Controllers\LembreteController;
+use Illuminate\Support\Facades\Log;
 
 class EnviarLembretesDiarios extends Command
 {
@@ -12,10 +13,16 @@ class EnviarLembretesDiarios extends Command
 
     public function handle(): void
     {
+        Log::info('[Lembretes] 📅 Executando lembretes:enviar (comando agendado iniciado)');
+
         $controller = new LembreteController();
         $resposta = $controller->enviarLembretesManualmente();
 
         $this->info('Envio de lembretes agendado:');
         dump($resposta->getContent());
+
+        Log::info('[Lembretes] ✅ Lembretes enviados com sucesso.', [
+            'resposta' => $resposta->getContent(),
+        ]);
     }
 }
