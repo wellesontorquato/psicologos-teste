4ff4a0fe711ffdde9605dcec5e0fbd20e380669e
wellesontorquato <welleson09@hotmail.com>
2025-05-05 13:26:17 -0300

    Add data_hora_original column to sessoes table

---
+++ database/migrations/2025_05_05_162511_add_data_hora_original_to_sessoes_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('sessoes', function (Blueprint $table) {
+            $table->dateTime('data_hora_original')->nullable()->after('data_hora');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('sessoes', function (Blueprint $table) {
+            $table->dropColumn('data_hora_original');
+        });
+    }
+};
