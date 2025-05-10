c517a238dbdd2e76549a1e57720442996327052f
wellesontorquato <welleson09@hotmail.com>
2025-05-10 17:36:03 -0300

    fix: verify.blade.php e button.blade.php

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -4,14 +4,11 @@
 
 Olá! Obrigado por se cadastrar em nossa plataforma.
 
-Por favor, clique no botão abaixo para verificar seu endereço de e-mail e ativar sua conta:
+Clique no botão abaixo para verificar seu e-mail e ativar sua conta:
 
-<p style="text-align: center; margin: 20px 0;">
-    <a href="{{ $verificationUrl }}"
-       style="background-color: #00aaff; color: #ffffff; padding: 12px 20px; text-decoration: none; border-radius: 5px; display: inline-block; font-weight: bold;">
-        Verificar E-mail
-    </a>
-</p>
+@component('mail::button', ['url' => $verificationUrl, 'color' => 'primary'])
+Verificar E-mail
+@endcomponent
 
 Se você não criou esta conta, nenhuma ação é necessária.
 
@@ -33,7 +30,7 @@ Se você não criou esta conta, nenhuma ação é necessária.
         </td>
         <td>
             <a href="https://x.com/psigestor" target="_blank">
-                <img src="https://www.psigestor.com/images/twitter.png" alt="Twitter" width="32" height="32">
+                <img src="https://www.psigestor.com/images/twitter.png" alt="Twitter / X" width="32" height="32">
             </a>
         </td>
     </tr>
