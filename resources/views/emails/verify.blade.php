9416fffe571e069dabbdfb0d38dbc74259904133
wellesontorquato <welleson09@hotmail.com>
2025-05-10 17:32:44 -0300

    fix 4: verify.blade.php

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -7,7 +7,7 @@ Olá! Obrigado por se cadastrar em nossa plataforma.
 Por favor, clique no botão abaixo para verificar seu endereço de e-mail e ativar sua conta:
 
 <p style="text-align: center; margin: 20px 0;">
-    <a href="{{ $url }}"
+    <a href="{{ $verificationUrl }}"
        style="background-color: #00aaff; color: #ffffff; padding: 12px 20px; text-decoration: none; border-radius: 5px; display: inline-block; font-weight: bold;">
         Verificar E-mail
     </a>
@@ -23,19 +23,20 @@ Se você não criou esta conta, nenhuma ação é necessária.
     <tr>
         <td>
             <a href="https://www.instagram.com/psigestor/" target="_blank">
-                <img src="https://psicologos-teste-production.up.railway.app/images/instagram.png" alt="Instagram" width="32" height="32" style="margin-right:10px;">
+                <img src="https://www.psigestor.com/images/instagram.png" alt="Instagram" width="32" height="32" style="margin-right:10px;">
             </a>
         </td>
         <td>
             <a href="https://www.facebook.com/psigestor/" target="_blank">
-                <img src="https://psicologos-teste-production.up.railway.app/images/facebook.png" alt="Facebook" width="32" height="32" style="margin-right:10px;">
+                <img src="https://www.psigestor.com/images/facebook.png" alt="Facebook" width="32" height="32" style="margin-right:10px;">
             </a>
         </td>
         <td>
             <a href="https://x.com/psigestor" target="_blank">
-                <img src="https://psicologos-teste-production.up.railway.app/images/twitter.png" alt="Twitter" width="32" height="32">
+                <img src="https://www.psigestor.com/images/twitter.png" alt="Twitter" width="32" height="32">
             </a>
         </td>
     </tr>
 </table>
+
 @endcomponent
