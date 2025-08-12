5cdf718fdc9aa71acf71c189bcd27ba1c4e30610
wellesontorquato <welleson09@hotmail.com>
2025-08-12 20:44:54 -0300

    Ajuste Google Ads

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -70,6 +70,17 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PTGJTGFPC5">
+    </script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-PTGJTGFPC5');
+    </script>
+
     {{-- Estilos Globais da Landing Page --}}
     <style>
         html, body {
