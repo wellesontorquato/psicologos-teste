16db6ded7cf68ad0bc9fb310c4080dd2816ac55e
Welleson Torquato <welleson@jardim.adm.local>
2025-12-26 12:44:06 -0300

    Ajuste de Backup completo

--- app/Console/Commands/BackupBancoMysql.php
+++ app/Console/Commands/BackupBancoMysql.php
@@ -40,6 +40,7 @@ class BackupBancoMysql extends Command
             '--single-transaction',
             '--quick',
             '--skip-lock-tables',
+            '--ssl=0', // compatível com client antigo no Railway
             '-h', $host,
             '-P', $port,
             '-u', $user,
