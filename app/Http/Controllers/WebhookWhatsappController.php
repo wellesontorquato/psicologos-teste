c9b134a957ac5607978fc2b65aa4b6872f68e483
Welleson Torquato <122334195+wellesontorquato@users.noreply.github.com>
2026-03-26 09:12:41 -0300

    Update WebhookWhatsappController.php

--- app/Http/Controllers/WebhookWhatsappController.php
+++ app/Http/Controllers/WebhookWhatsappController.php
@@ -447,7 +447,7 @@ class WebhookWhatsappController extends Controller
                 'fromMe' => false,
                 'type'   => 'chat',
                 'id'     => [
-                    '_serialized' => 'TESTE_MANUAL_1234567891',
+                    '_serialized' => 'CATCHAU',
                 ],
             ],
         ];
