191a57fb1f5715a977158c62a8b4e3a6185813b2
Welleson Torquato <122334195+wellesontorquato@users.noreply.github.com>
2026-04-01 18:05:18 -0300

    Update WebhookWhatsappController.php

--- app/Http/Controllers/WebhookWhatsappController.php
+++ app/Http/Controllers/WebhookWhatsappController.php
@@ -442,12 +442,12 @@ class WebhookWhatsappController extends Controller
         $dados = [
             'event' => 'onmessage',
             'data'  => [
-                'from'   => '5538998278608@c.us',
+                'from'   => '5538998161044@c.us',
                 'body'   => '1',
                 'fromMe' => false,
                 'type'   => 'chat',
                 'id'     => [
-                    '_serialized' => 'CATCHAU1234567',
+                    '_serialized' => 'CATCHAU12345678',
                 ],
             ],
         ];
